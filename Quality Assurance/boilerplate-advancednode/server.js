e43e171478f20531e8346687faadb2a72631ea2b
unknown <428230171@qq.com>
2021-04-12 19:18:16 +0800

    summary and think

---
+++ Quality Assurance/boilerplate-advancednode/server.js
@@ -0,0 +1,53 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+
+const pug = require('pug');
+const passport = require('passport');
+const session = require('express-session');
+
+const app = express();
+
+
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: true,
+//     saveUninitialized: true,
+//     cookie: { secure: false },
+//   })
+// );
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('view engine', 'pug');
+const ObjectID = require("mongodb").ObjectID;
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+  //   done(null, null);
+  // });
+  done(null, null);
+});
+
+app.route('/').get((req, res) => {
+  res.render(process.cwd() + "/views/pug/index", {
+    title: "Hello",
+    message: "Please login",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT);
+});
